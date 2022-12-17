a50477139da01eeeac130e4a85771e70ed801da3
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-17 20:16:57 +0530

    new

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-function save(e){
-   e.preventDefault();
-const expenseAmount = document.getElementById('expenseamount').value;
-const description = document.getElementById('description').value;
-const category = document.getElementById('category').value;
-const obj={
-    expenseAmount,
-    description,
-    category
-}
-localStorage.setItem(obj.description,JSON.stringify(obj));
- showExpenseDetails(obj);
+function save(event) {
+    event.preventDefault();
+    const expenseAmount = document.getElementById('expenseamount').value;
+    const description = document.getElementById('description').value;
+    const category = document.getElementById('category').value;
+    const obj = {
+        expenseAmount,
+        description,
+        category
+    }
+    localStorage.setItem(obj.description, JSON.stringify(obj));
+    showExpenseDetails(obj);
 }
 window.addEventListener('DOMContentLoaded', (event) => {
+    console.log("test");
     Object.keys(localStorage).forEach(key => {
         const user = JSON.parse(localStorage.getItem(key))
-        showExpenseDetails(user)
+        showExpenseDetails(user);
     })
 })
 
@@ -25,23 +26,23 @@ function showExpenseDetails(user) {
     <button onclick=editUserDetails('${user.description}','${user.expenseAmount}','${user.category}','${user._id}')>Edit User </button>
  </li>`
     parentNode.innerHTML = parentNode.innerHTML + childHtml;
-    
+
 }
 function deleteUser(description) {
     localStorage.removeItem(description)
     removeItemFromScreen(description)
 }
 
-function removeItemFromScreen(description){
+function removeItemFromScreen(description) {
     const parentNode = document.getElementById('listOfitems');
     const elem = document.getElementById(description)
     parentNode.removeChild(elem);
 }
 
-function editUserDetails(description, expenseAmount, category, userId){
+function editUserDetails(description, expenseAmount, category, userId) {
 
     document.getElementById('description').value = description;
     document.getElementById('expenseAmount').value = expenseAmount;
-    document.getElementById('category').value =category;
+    document.getElementById('category').value = category;
     deleteUser(userId)
 }
