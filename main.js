3801245c4b398272d1c4ec2ad4592d6ca4b9fa94
umeshuy <110802360+umeshuy@users.noreply.github.com>
2022-12-22 09:56:55 +0530

    final

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ expForm.addEventListener("submit", save)
 
 function save(event) {
     event.preventDefault();
-    console.log(event)
-    const expenseAmount = document.getElementById('expenseamount').value;
+    // console.log(event)
+    const expenseAmount = document.getElementById('expenseAmount').value;
     const description = document.getElementById('description').value;
     const category = document.getElementById('category').value;
     const obj = {
@@ -18,7 +18,6 @@ function save(event) {
     showExpenseDetails(obj);
 }
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log("test");
     Object.keys(localStorage).forEach(key => {
         const user = JSON.parse(localStorage.getItem(key))
         showExpenseDetails(user);
@@ -29,26 +28,31 @@ function showExpenseDetails(user) {
     const parentNode = document.getElementById('listofusers');
     const childHtml = `<li id=${user.description}> ${user.expenseAmount} - ${user.description}-'${user.category}'
     <button onclick=deleteUser('${user.description}')> Delete User </button>
-    <button onclick=editUserDetails('${user.description}','${user.expenseAmount}','${user.category}','${user._id}')>Edit User </button>
+    <button onclick=editUserDetails('${user.description}','${user.expenseAmount}','${user.category}')>Edit User </button>
  </li>`
     parentNode.innerHTML = parentNode.innerHTML + childHtml;
 
 }
+
+
+function removeItemFromScreen(description) {
+    const parentNode = document.getElementById('listOfusers');
+    const elem = document.getElementById(description)
+    parentNode.removeChild(elem);
+}
+
+
 function deleteUser(description) {
     localStorage.removeItem(description)
     removeItemFromScreen(description)
 }
 
-function removeItemFromScreen(description) {
-    const parentNode = document.getElementById('listOfitems');
-    const elem = document.getElementById(description)
-    parentNode.removeChild(elem);
-}
 
-function editUserDetails(description, expenseAmount, category, userId) {
+
+function editUserDetails(description, expenseAmount, category) {
 
     document.getElementById('description').value = description;
     document.getElementById('expenseAmount').value = expenseAmount;
     document.getElementById('category').value = category;
-    deleteUser(userId)
+    deleteUser(description)
 }
